c01b150b880eb1398794dbdb2e96fcee7ffa4736
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-25 01:48:52 +0200

    start building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/homePage/Hero";
 import { Informations } from "@/components/informations/Informations";
+import { StartBuilding } from "@/components/startBuilding/StartBuilding";
 
 export default function Home() {
   return (
   <>
     <Hero />
     <Informations />
+    <StartBuilding />
   </>
   );
 }
---
+++ src/components/startBuilding/StartBuilding.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../ui/button"
+
+export const StartBuilding = () => {
+  return <section className="max-w-[1200px] mx-auto px-4 py-16 mb-5">
+    <div className="flex items-center flex-col justify-center gap-4">
+        <h1 className="text-4xl bg-clip-text text-transparent font-extrabold bg-gradient-to-r from-red-400 to-pink-600">Start building your course today</h1>
+        <Button className="bg-[#E11D48] hover:bg-[#E11D48]/80 text-white font-bold text-xl p-8 uppercase">Build Your First Course</Button>
+    </div>
+  </section>
+}
