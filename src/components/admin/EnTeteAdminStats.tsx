8b50dc9f3ee05dafeae0a19cc546d7a307496cd7
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-25 20:09:51 +0200

    better BreadCrumb

--- src/components/admin/EnTeteAdminStats.tsx
+++ src/components/admin/EnTeteAdminStats.tsx
@@ -1,13 +1,22 @@
 "use client"
 import { useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 
 export const EnTeteAdminStats = () => {
     const router = useRouter()
-  return <div className="border-b border-gray-800 py-1">
+  return <div className="border-b border-gray-800 py-3">
     <div className="max-w-[800px] flex items-center gap-5 mx-auto">
-        <Button onClick={() => router.back()} variant="default" className="text-xs bg-transparent hover:bg-[#111111]">Back</Button>
-        <h1 onClick={() => router.push("/admin")} className="text-xs cursor-pointer hover:text-white duration-75">Admin</h1>
+    <Breadcrumb>
+  <BreadcrumbList className="text-xs text-white">
+    <BreadcrumbItem>
+      <BreadcrumbLink className="cursor-pointer text-gray-400 hover:text-white duration-75" onClick={() => router.back()}>Back</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator className="text-gray-400"/>
+    <BreadcrumbItem>
+      <BreadcrumbLink className="cursor-pointer text-gray-400 hover:text-white duration-75" onClick={() => router.push('/admin')}>Admin</BreadcrumbLink>
+    </BreadcrumbItem>
+  </BreadcrumbList>
+</Breadcrumb>
     </div>
   </div>
 }
