95346ce61bc7ac5be2f02ec1a45674cfb7822092
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-25 13:33:15 +0200

    navigation

--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -1,22 +1,42 @@
-import { Moon } from "lucide-react"
+"use client"
+import { Moon, Menu, X } from "lucide-react"
 import { Authentication } from "./Authentication"
 import { Links } from "./Links"
 import Link from "next/link"
+import { useState } from "react"
+
 export const Headers = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return <header className="h-16 border-b border-gray-800">
     <nav className="flex items-center justify-between h-full w-full max-w-[1400px] mx-auto px-4">
         <div className="flex items-center gap-2">
-        <Link href="/" className="flex items-center gap-2">
-            <img src="/logo.svg" alt="logo" className="w-12 h-12" />
-            <h2 className="text-xl font-semibold text-white">YouCode</h2>
-        </Link>
-        <Links />
+          <Link href="/" className="flex items-center gap-2">
+              <img src="/logo.svg" alt="logo" className="w-12 h-12" />
+              <h2 className="text-xl font-semibold text-white">YouCode</h2>
+          </Link>
+          <div className="hidden md:block">
+            <Links />
+          </div>
         </div>
 
         <div className="flex items-center gap-3">
             <Authentication />
             <Moon className="w-5 cursor-pointer h-5"/>
+            <Menu 
+              className="w-6 h-6 cursor-pointer md:hidden" 
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
         </div>
     </nav>
+    <div className={`md:hidden fixed inset-0 bg-gray-900 bg-opacity-90 z-50 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+      <div className={`flex flex-col items-center justify-center h-full transition-transform duration-300 ${isMenuOpen ? 'translate-y-0' : '-translate-y-full'}`}>
+        <X 
+          className="absolute top-4 right-4 w-6 h-6 cursor-pointer text-white"
+          onClick={() => setIsMenuOpen(false)}
+        />
+        <Links />
+      </div>
+    </div>
   </header>
 }
--- src/components/headers/Links.tsx
+++ src/components/headers/Links.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
-export const Links = () => {
-  return <div className="flex items-center gap-4 ml-3">
+export const Links: React.FC = () => {
+  return <div className="flex md:flex-row flex-col items-center gap-4 md:ml-3">
     <Link href="/" className="hover:text-white text-md hover:underline">Explorer</Link>
     <Link href="/" className="hover:text-white text-md hover:underline">Courses</Link>
   </div>
