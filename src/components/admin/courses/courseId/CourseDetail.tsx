646ca2f0ea515716ad1a3b3ef5e924f14158d645
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-26 15:24:40 +0200

    users

--- src/components/admin/courses/courseId/CourseDetail.tsx
+++ src/components/admin/courses/courseId/CourseDetail.tsx
@@ -1,5 +1,14 @@
 export const CourseDetail = ({course}: {course: any}) => {
-  return <>
-    
-  </>
+  return <div className="max-w-[750px] h-[600px] mx-auto">
+    <h2 className="text-2xl font-bold border-b text-white border-gray-800 py-2 mb-5 w-fit">Course</h2>
+
+    <div className="flex gap-5 mb-5">
+        <div className="w-[60%] bg-[#1C1917] border border-gray-800 p-5 rounded-lg">
+        
+        </div>
+        <div className="w-[40%] bg-[#1C1917] border border-gray-800 p-5 rounded-lg">
+
+        </div>
+    </div>
+  </div>
 }
