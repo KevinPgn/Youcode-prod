da0cbf9127bd124d088672c2215cbf6f24fa1c89
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-27 13:01:06 +0200

    new mdx

--- src/components/admin/courses/courseId/chapters/chapterId/ContenuChapter.tsx
+++ src/components/admin/courses/courseId/chapters/chapterId/ContenuChapter.tsx
@@ -1,5 +1,8 @@
+import { MDXEditor } from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+
 export const ContenuChapter = ({chapter}: {chapter: any}) => {
   return <div className="flex-1 bg-[#1C1917] rounded-md p-5">
-    
+    <h2 className="text-md font-normal text-white mb-5">Content</h2>
   </div>
 }
